2e533865072c1871bfbc45bd86e0cf4524fbf72e
Samuel Andrés <samuelandres@yandex.com>
2024-07-17 22:42:28 +0200

    doc(learning-ri): update doc

--- learning-ri/build.sh
+++ learning-ri/build.sh
@@ -2,7 +2,7 @@ rm -rf bin/ lib/ CMakeFiles/ CMakeCache.txt Makefile cmake_install.cmake
 cmake . -G "CodeBlocks - Unix Makefiles"
 make
 make test
-ctest --output-on-failure
+ctest -V --output-on-failure
 bin/polymorphism-demo
 bin/array-demo
 bin/unit-simple-ri-demo
